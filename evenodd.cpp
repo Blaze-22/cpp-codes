d324ec4e50b7a8a03ab432874628b1c5aa1dd5ad
Swastik Malke <swastikmalke@gmail.com>
2026-03-20 11:16:37 +0530

    Add program to check if a number is even or odd

---
+++ evenodd.cpp
@@ -0,0 +1,15 @@
+//WAC++P to check whether a number is even or odd using if-else.
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"Odd";
+    }
+    return 0;
+}
